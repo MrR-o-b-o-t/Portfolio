84d6f3a1203d2983c1c47c83b66d3e346d964b22
Web-Dev-Jr <50638220+Web-Dev-Jr@users.noreply.github.com>
2019-12-15 22:06:06 -0600

    changed footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,6 +4,8 @@ class Footer extends Component {
   render() {
 
     if(this.props.data){
+      var {name} = this.props.data;
+      var currentYear = new Date().getFullYear();
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -19,8 +21,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+  <li>&copy; Copyright {currentYear} {name}</li>
            </ul>
 
         </div>
